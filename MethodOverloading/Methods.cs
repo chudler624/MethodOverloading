def863f08717c483d39418e7b703201d212cc40e
Casey Hudler <chudler624@gmail.com>
2022-08-15 16:10:06 -0400

    initial push

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+
+
+
+        public static int Add(int x, int y) 
+        {
+            return x + y; 
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y; 
+        }
+
+        public static string Add(int x, int y, bool yes)
+        {
+            
+
+            var sum = x + y;
+
+            if (yes && sum == 1)
+                return $"{sum}+dollar";
+            else if (yes && sum >= 2)
+                return $"{sum}+dollars";
+            else 
+                return (x + y).ToString();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,16 @@
+﻿using MethodOverloading;
+
+var answer = Methods.Add(1, 2);
+Console.WriteLine(answer);
+
+
+var answer2= Methods.Add(2.5m, 7.3m);
+Console.WriteLine(answer2);
+
+
+var answer3 = Methods.Add(2, 1, true);
+Console.WriteLine(answer3);
+
+
+var answer4 = Methods.Add(2, 1, false);
+Console.WriteLine(answer4);
